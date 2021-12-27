b39a2703a06ac6dea2b4cc11df10a99461f61a6d
Ashraf Ben Slimane <benslimaneashraf@gmail.com>
2021-12-27 16:04:57 +0100

    create loading-scrren

--- src/components/LoadingScreen/index.jsx
+++ src/components/LoadingScreen/index.jsx
@@ -1,5 +1,11 @@
+import LOGO from 'src/assets/images/logo/Lockup-Color.svg';
+
 const LoadingScreen = () => {
-  return <>Loading</>;
+  return (
+    <div className="loading-screen">
+      <img className="logo" alt="Logo of our website" src={LOGO} />
+    </div>
+  );
 };
 
 export default LoadingScreen;
